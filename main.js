3dc2c46291868a13f85d1333d686ebc20c53f452
Suman293 <75357331+Suman293@users.noreply.github.com>
2021-04-22 10:54:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var SpeechRecognization = window.webkitSpeechRecognisation;
+
+var recognization = new SpeechRecognization
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognization.start();
+}
+
+recognization.onresult = function(event) {
+    console.log(event);
+
+    var Content = event.results[0] [0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+       speak();
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").innerHTML;
+
+    var utter This = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+}
+
+camera = document.getElementById("camera")
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+})
